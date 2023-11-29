c0296e4b041ff764e089c1bb1e545421f93cca83
Tyler Oreskey <tyleroreskey@Tylers-MacBook-Pro-6.local>
2023-11-28 18:32:57 -0700

    added cyan start/stop text to keylogger. The default text color is white unless specified. Added commented out vm configuration to config.

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# Config for local
 CLIENT_HOST = 'localhost'
 CLIENT_PORT = 12346
 SERVER_HOST = 'localhost'
 SERVER_PORT = 12346
+
+# # Config for VM
+# CLIENT_HOST = 'localhost'
+# CLIENT_PORT = 12345
+# SERVER_HOST = '0.0.0.0'
+# SERVER_PORT = 12345
+
 KEYLOGGER_OPTION = '2'
